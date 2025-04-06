0f5ff28473288a61766758d279a575e9b36c59d7
shayne song <qiangsong889@yahoo.com>
2025-04-05 18:58:21 -0700

    Initial Commit

---
+++ Chilesha/ChileshaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChileshaApp.swift
+//  Chilesha
+//
+//  Created by shayne song on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ChileshaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
